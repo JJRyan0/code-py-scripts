d4c4f77b2046739426a2e705b985f179f54c72f1
John Ryan <johnjoseph.ryan0@gmail.com>
2021-02-24 00:27:23 +0000

    Add files via upload

---
+++ Luggage.py
@@ -0,0 +1,5 @@
+#program to take user input to measure weight of luggage bag
+weight = float(raw_input("How many kg does you bag weigh? "))
+if weight > 15:
+    print("You exceeded the baggage limit of 15 kg")
+print("Thank you.")
